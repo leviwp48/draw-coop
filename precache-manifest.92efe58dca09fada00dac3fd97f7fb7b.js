cc488be1da74540631bea0ba037f37101addc317
Levi Pole <leviwp48@gmail.com>
2021-02-01 10:05:44 -0800

    Updates

---
+++ precache-manifest.92efe58dca09fada00dac3fd97f7fb7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c5c7c656661d0609d1597ed6d3aad1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d3a94d7e324d239cf5e4",
+    "url": "/my-app/static/css/main.704ac6b7.chunk.css"
+  },
+  {
+    "revision": "29f13714ea7d28313621",
+    "url": "/my-app/static/js/2.28a9188d.chunk.js"
+  },
+  {
+    "revision": "b91da89a3bf523d6267218a2eaa9bd69",
+    "url": "/my-app/static/js/2.28a9188d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a94d7e324d239cf5e4",
+    "url": "/my-app/static/js/main.6dbc6e59.chunk.js"
+  },
+  {
+    "revision": "0614ea3f126a3f1ffde6",
+    "url": "/my-app/static/js/runtime-main.51ad11b3.js"
+  }
+]);
