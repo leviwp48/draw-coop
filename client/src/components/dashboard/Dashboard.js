ea693b9ba7583b86838c4cf02903f9f44bd23b39
Levi <leviwp48@gmail.com>
2021-06-16 21:13:26 -0700

    Updating UX and combining components

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import Board from "../board/Board";
 import Chat from "../chat/Chat"
 import jwt_decode from 'jwt-decode';
 import socketIOClient from "socket.io-client";
+import Modal from "../login/LoginModal.js";
 const ENDPOINT = "http://127.0.0.1:3001";
 
 export default class Dashboard extends Component {
@@ -24,7 +25,11 @@ export default class Dashboard extends Component {
       display: [],
       boardImage: "",
       boardState: [],
-      socket: socketIOClient(ENDPOINT)
+      socket: socketIOClient(ENDPOINT),
+      show: false,
+      modalType: true,
+      username: "",
+      password: "",
     }
   }
 
@@ -103,6 +108,94 @@ export default class Dashboard extends Component {
       });
   }
   
+  showModalLogin = () => {
+    this.setState({
+      show: true,
+      modalType: true
+    });
+  };
+
+  showModalRegister = () => {
+    this.setState({
+      show: true,
+      modalType: false
+    });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  handleSubmitLogin = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    
+    axios.post(`http://localhost:3001/api/users/login`, user)
+      .then(res => {
+        this.props.setUsername(jwt_decode(res.data.token).username);
+        this.props.setToken(res.data.token);
+        this.setState({show: false});
+        sessionStorage.setItem('user', res.data.token)
+        
+      })     
+      .catch(err => {
+        console.log(err.response)
+      });
+
+      
+  }
+
+  handleSubmitRegister = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    
+
+    axios.post(`http://localhost:3001/api/users/register`, user)
+      .then(res => {
+      
+        this.setState({show: false});
+        axios.post(`http://localhost:3001/api/users/login`, user)
+        .then(res => {
+          this.props.setToken(res.data.token);
+          this.props.setUsername(jwt_decode(res.data.token).username);
+          this.setState({show: false});
+        })
+        .catch(err => {
+          console.log(err.response)
+        });
+      })
+      .catch(err => {
+        console.log(err.response)
+      });
+  }
+
+  onEnter = (e) => {
+    if (e.key === "Enter"){
+      if(this.state.modalType === true){
+        this.handleSubmitLogin(e)
+      }
+      else{
+        this.handleSubmitRegister(e)
+      }
+    } 
+  }
+
+  handleUsernameChange = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  };
+
   setDisplay = () => {
     if(this.state.showBoard && this.props.getTokenStatus() == true){
       return this.state.display
@@ -117,15 +210,34 @@ export default class Dashboard extends Component {
     }
     else{
       return (
+        <>
         <div className="intro-container">   
           <div className="intro">
             <p> Drawing stuff is better with friends </p>
           </div>
-          <div className="intro-login">
-            Register or login to get started
-          </div>
+          
         </div>
-
+        <div className="intro-login">
+          <button
+            className="dashboard-button-login"
+            id="dashboard-user"
+            type="button"
+            onClick={() => this.showModalLogin()}
+          >
+          Login
+          </button>
+          or
+          <button
+            className="dashboard-button-register"
+            id="dashboard-user"
+            type="button"
+            onClick={() => this.showModalRegister()}
+          >
+          Register
+          </button>
+          to start drawing
+        </div>
+        </>
       );
     }
   }
@@ -134,12 +246,45 @@ export default class Dashboard extends Component {
   render() {
   
     return (
-      <div>        
-        <Nav setUsername={this.props.setUsername} getUsername={this.props.getUsername} 
-             setToken={this.props.setToken} getToken={this.props.getToken} getTokenStatus={this.props.getTokenStatus}
-             deleteToken={this.props.deleteToken} setUserId={this.setUserId} setBoardData={this.setBoardData}/>
-        {this.setDisplay()}
-             
+      <div>
+        <Modal 
+          ref={node => this.node = node}
+          show={this.state.show}
+          submitRegister={this.handleSubmitRegister}
+          submitLogin={this.handleSubmitLogin}
+          onEnter={this.onEnter}
+          modalType={this.state.modalType}
+          handleClose={this.hideModal}
+        >
+          <input
+            className="input-username"
+            id="input"
+            type="text"
+            placeholder="username"
+            value={this.username}
+            onChange={this.handleUsernameChange}
+            onKeyPress={this.onEnter}
+          />
+          <input
+            className="input-password"
+            id="input"
+            type="text"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+            onKeyPress={this.onEnter}
+          />
+        </Modal>
+        <div>        
+          <Nav setUsername={this.props.setUsername} getUsername={this.props.getUsername} 
+              setToken={this.props.setToken} getToken={this.props.getToken} getTokenStatus={this.props.getTokenStatus}
+              deleteToken={this.props.deleteToken} setUserId={this.setUserId} setBoardData={this.setBoardData}/>
+          {this.setDisplay()}
+          
+        </div>
+        <div className="outro-container">
+          <p className="outro-message"> Thanks for coming! </p>
+        </div>
       </div>
     );
   }
